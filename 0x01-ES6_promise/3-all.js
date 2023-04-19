afdee28b895041bf8ea8ded2348c01ab18d6254a
Joseph Alikah <alikahjoseph@gmail.com>
2023-04-19 22:19:17 +0100

    0x01 - Add task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,5 +5,7 @@ export default function handleProfileSignup() {
     uploadPhoto().then((photo) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     });
+  }).catch(() => {
+    console.log('Signup system offline');
   });
 }
