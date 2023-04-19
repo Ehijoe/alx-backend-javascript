4c642dc3b69ae018eaf69f7cf43778517e129115
Joseph Alikah <alikahjoseph@gmail.com>
2023-04-19 22:05:14 +0100

    0x01 - Add task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  createUser().then((user) => {
+    uploadPhoto().then((photo) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    });
+  });
+}
