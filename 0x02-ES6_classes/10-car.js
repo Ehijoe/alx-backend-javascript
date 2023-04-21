90d5445eddc192a1bac28ff9b4134a3ca94b6027
Joseph Alikah <alikahjoseph@gmail.com>
2023-04-21 02:44:29 +0100

    0x02 - Add task 11

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,6 +5,10 @@ export default class Car {
     this._color = color;
   }
 
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
     const constructor = this.constructor[Symbol.species];
     return new constructor(this.brand, this.motor, this.color);
