e15b510b66a1ef858b0d1d33bc0e98b4ae842633
Joseph Alikah <alikahjoseph@gmail.com>
2023-04-24 23:12:57 +0100

    0x03 - Fix task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,6 @@
 export default function cleanSet(dirtySet, startString) {
   return Array.from(dirtySet)
     .filter((word) => (word.startsWith(startString)))
+    .map((word) => (word.substring(startString.length)))
     .join('-');
 }
