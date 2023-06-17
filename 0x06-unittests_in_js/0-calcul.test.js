46a7a7a6eb069b6e148a7f616fac95da8a91fb01
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-17 02:32:36 +0100

    0x06 - Correct imports

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,17 +1,18 @@
-const assert = require("assert");
-const calculateNumber = require("./0-calcul");
+const calculateNumber = require('./0-calcul.js');
+const { it, describe } = require('mocha');
+const assert = require('assert');
 
-assert.equal(calculateNumber(1.0, 0), 1);
-assert.equal(calculateNumber(1.3, 0), 1);
-assert.equal(calculateNumber(1.7, 0), 2);
-
-assert.equal(calculateNumber(0, 1.0), 1);
-assert.equal(calculateNumber(0, 1.3), 1);
-assert.equal(calculateNumber(0, 1.7), 2);
-
-assert.equal(calculateNumber(1.3, 0), 1);
-assert.equal(calculateNumber(0, 1.2), 1);
-assert.equal(calculateNumber(1.3, 1.3), 2);
-assert.equal(calculateNumber(1.7, 1.2), 3);
-assert.equal(calculateNumber(1.3, 1.8), 3);
-assert.equal(calculateNumber(1.6, 1.8), 4);
+describe('calculateNumber', () => {
+  it('should return 4 when called with 1 and 3', () => {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+  it('should return 5 when called with 1 and 3.7', () => {
+    assert.equal(calculateNumber(1, 3.7), 5);
+  });
+  it('should return 5 when called with 1.2 and 3.7', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return 6 when called with 1.5 and 3.7', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
+});
