484ab4c020955d41bdd2dc468b3ea2f6ed3eb990
Joseph Alikah <alikahjoseph@gmail.com>
2023-06-17 02:38:41 +0100

    0x06 - Remove arrow functions

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,17 +2,17 @@ const calculateNumber = require('./0-calcul.js');
 const { it, describe } = require('mocha');
 const assert = require('assert');
 
-describe('calculateNumber', () => {
-  it('should return 4 when called with 1 and 3', () => {
+describe('calculateNumber', function () {
+  it('should return 4 when called with 1 and 3', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
   });
-  it('should return 5 when called with 1 and 3.7', () => {
+  it('should return 5 when called with 1 and 3.7', function () {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
-  it('should return 5 when called with 1.2 and 3.7', () => {
+  it('should return 5 when called with 1.2 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
   });
-  it('should return 6 when called with 1.5 and 3.7', () => {
+  it('should return 6 when called with 1.5 and 3.7', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
